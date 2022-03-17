0b64369b8fab1dc0fcd225dfddd76dbe8290f26a
Irina0710 <defmail2me@gmail.com>
2022-03-17 16:33:20 +0300

    Task3

---
+++ python2_1.py
@@ -0,0 +1,14 @@
+# найти максимальное из 5 чисел
+print ('Введите первое число')
+a = int(input())
+print ('Введите второе число')
+b = int(input())
+print ('Введите третье число')
+с = int(input())
+print ('Введите четвертое число')
+d= int(input())
+print ('Введите пятое число')
+e= int(input())
+list = [a, b, с, d, e]
+max_number = max(list)
+print (max_number)

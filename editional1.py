b5649e9a7b70eee39f3c1dc65478c3092d4ed604
Irina0710 <defmail2me@gmail.com>
2022-03-20 22:36:45 +0300

    Task_editional1_1

--- editional1.py
+++ editional1.py
@@ -1,6 +1,10 @@
 # Сделайте так, чтобы число секунд отображалось в в иде дни:часы:минуты:секунды
 Sec = int(input('введите число секунд '))
-Min = Sec / 60
-Hour = Min / 60
-Day = Hour / 24
-print(f'В {Sec} секундах {Min} минут, {Hour} часов и {Day} дней')
+Days = Sec // 86400
+rest = Sec % 86400
+Hours = rest // 3600
+rest = rest % 3600
+minutes = rest // 60
+seconds = rest % 60
+print(f'{Days} days, {Hours} hours, {minutes} minutes, {seconds} seconds')
+

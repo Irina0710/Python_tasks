6743da4816e9777d8379f6e48da93da9ffa259b5
Irina0710 <defmail2me@gmail.com>
2022-03-26 18:39:04 +0300

    task20_edit

--- python20.py
+++ python20.py
@@ -1,13 +1,9 @@
 #Определить, присутствует ли в заданном списке строк, некоторое число 
-string_list = ['hello', '123', 'world', '34kdh']
-item_to_find = input ('Введите число')
-flag = True
-for i in string_list:
-    if flag == False:
-        break
-    elif i == item_to_find:
-        print(f"Искомое число {item_to_find} есть в списке")
-        flag = False
-if flag==True:
-    print(f'Искомое число {item_to_find} отсутствует в списке')
+n = int(input('Ввведите  число:  '))
+List = ['hello', '123', 'world', '34kdh']
+for n in List:
+    if n.isdigit():
+        print(n)
+
+
 
